c0d49b43f4ec6cad8249cbfe4ae778d1f3c165cd
DmitryShcherba <debaucep@yandex.ru>
2023-02-08 18:11:29 +0300

    add button, add label, add counter +1

--- MultipleVC_Github_test/ViewController.swift
+++ MultipleVC_Github_test/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.text = "Count: \(count)"
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        count += 1
+        
+        label.text = "Count: \(count)"
+    }
+    
 }
 
