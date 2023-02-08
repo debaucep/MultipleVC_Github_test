14f5d7563f23b03c1fff6668367ff3b09d623d97
DmitryShcherba <debaucep@yandex.ru>
2023-02-08 18:31:35 +0300

    add reset button

--- MultipleVC_Github_test/ViewController.swift
+++ MultipleVC_Github_test/ViewController.swift
@@ -13,18 +13,29 @@ class ViewController: UIViewController {
     
     
     @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         label.text = "Count: \(count)"
+        startButton.setTitle("Click", for: .normal)
+        
     }
 
     @IBAction func buttonPressed(_ sender: UIButton) {
         count += 1
-        
         label.text = "Count: \(count)"
     }
+    
+    @IBAction func resetPressed(_ sender: UIButton) {
+        
+        count = 0
+        label.text = "Count: \(count)"
+        
+    }
+    
     
 }
 
